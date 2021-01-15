7a71b356472bf905213643aaf587e2f5e06e4b95
erkitammeaid <erkipoiss01@gmail.com>
2021-01-15 11:06:59 +0200

    update

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const container = document.getElementById("container");
 
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
-  };
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+    };
 };
-
 makeRows(10, 10);
+
+const opponentContainer = document.getElementById("container")
+const containerClone = opponentContainer.cloneNode(true)
+
+const enemy = document.body.appendChild(containerClone)
+enemy.id = "opponentContainer"
