fd0838b75c6f235eca68b3b44e64d288e17c8c8f
erkitammeaid <erkipoiss01@gmail.com>
2021-01-29 11:32:14 +0200

    mitte midagi ei tööta

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/ships.html');
 });
+
+app.use('/javascripts', express.static(__dirname + '/javascripts'));
+app.use('/stylesheets', express.static(__dirname + '/stylesheets'));
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
+        io.emit('chat message', msg);
     });
-  });
+});
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(8000, () => {
+    console.log('listening on *:8000');
 });
